ca0c600848297880626d9575871dbaea5c6fa13a
Malavika Shamesh <malavika.shamesh@allianz.com>
2026-07-09 21:43:21 +0530

    Fix scroll-to-top on all screens - remove height:100% scroll ambiguity, reset all scroll containers

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,7 +4,10 @@ import { useLocation } from 'react-router-dom'
 export default function ScrollToTop() {
   const { pathname } = useLocation()
   useEffect(() => {
+    // Cover all possible scroll containers across browsers
     window.scrollTo(0, 0)
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
   }, [pathname])
   return null
 }
